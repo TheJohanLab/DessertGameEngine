42562ec5922cbd4018d17f717dc4b1ecf75f06bc
JohanLab <johan.labrado@hotmail.fr>
2024-07-14 19:09:00 +0200

    Add batch rendering data statistics

--- DessertGameEngine/src/Dessert/Renderer/Renderer2D.cpp
+++ DessertGameEngine/src/Dessert/Renderer/Renderer2D.cpp
@@ -20,9 +20,9 @@ namespace Dessert {
 
 	struct Renderer2DData
 	{
-		const uint32_t maxQuads = 10000;
-		const uint32_t maxVertices = maxQuads * 4;
-		const uint32_t maxIndices = maxQuads * 6;
+		static const uint32_t maxQuads = 20000;
+		static const uint32_t maxVertices = maxQuads * 4;
+		static const uint32_t maxIndices = maxQuads * 6;
 		static const uint32_t maxTextureSlots = 32;
 
 
@@ -38,6 +38,11 @@ namespace Dessert {
 
 		std::array<Ref<Texture2D>, maxTextureSlots> TextureSlots;
 		uint32_t TextureSlotIndex = 1; // 0 is for the white texture
+
+		glm::vec4 QuadVertexPositions[4];
+
+		Renderer2D::DataStatistics Stats;
+
 	};
 
 	static Renderer2DData s_SceneData;
@@ -105,6 +110,11 @@ namespace Dessert {
 		memset(s_SceneData.TextureSlots.data(), 0, s_SceneData.TextureSlots.size() * sizeof(uint32_t));
 		s_SceneData.TextureSlots[0] = s_SceneData.WhiteTexture;
 
+		s_SceneData.QuadVertexPositions[0] = { -0.5f, -0.5f, 0.0f, 1.0f };
+		s_SceneData.QuadVertexPositions[1] = {  0.5f, -0.5f, 0.0f, 1.0f };
+		s_SceneData.QuadVertexPositions[2] = {  0.5f,  0.5f, 0.0f, 1.0f };
+		s_SceneData.QuadVertexPositions[3] = { -0.5f,  0.5f, 0.0f, 1.0f };
+
 	}
 
 	void Renderer2D::Shutdown()
@@ -147,6 +157,17 @@ namespace Dessert {
 
 
 		RenderCommand::DrawIndexed(s_SceneData.QuadVertexArray, s_SceneData.quadIndexCount);
+		s_SceneData.Stats.DrawCalls++;
+	}
+
+	void Renderer2D::FlushAndReset()
+	{
+		EndScene();
+
+		s_SceneData.quadIndexCount = 0;
+		s_SceneData.QuadVertexBufferPtr = s_SceneData.QuadVertexBufferBase;
+
+		s_SceneData.TextureSlotIndex = 1;
 	}
 
 	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
@@ -159,31 +180,37 @@ namespace Dessert {
 	{
 		DGE_PROFILE_FUNCTION();
 
+		if (s_SceneData.quadIndexCount >= Renderer2DData::maxIndices)
+			FlushAndReset();
+
 		const float texIndex = 0.0f; // white texture
 		const float scale = 1.0f;
 
-		s_SceneData.QuadVertexBufferPtr->Position = position;
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
+			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[0];
 		s_SceneData.QuadVertexBufferPtr->Color = color;
 		s_SceneData.QuadVertexBufferPtr->TexCoord = { 0.0f, 0.0f };
 		s_SceneData.QuadVertexBufferPtr->TexIndex = texIndex;
 		s_SceneData.QuadVertexBufferPtr->Scale = scale;
 		s_SceneData.QuadVertexBufferPtr++;
 
-		s_SceneData.QuadVertexBufferPtr->Position = { position.x + size.x, position.y, 0.0f };
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[1];
 		s_SceneData.QuadVertexBufferPtr->Color = color;
 		s_SceneData.QuadVertexBufferPtr->TexCoord = { 1.0f, 0.0f };
 		s_SceneData.QuadVertexBufferPtr->TexIndex = texIndex;
 		s_SceneData.QuadVertexBufferPtr->Scale = scale;
 		s_SceneData.QuadVertexBufferPtr++;
 
-		s_SceneData.QuadVertexBufferPtr->Position = { position.x + size.x, position.y + size.y, 0.0f };
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[2];
 		s_SceneData.QuadVertexBufferPtr->Color = color;
 		s_SceneData.QuadVertexBufferPtr->TexCoord = { 1.0f, 1.0f };
 		s_SceneData.QuadVertexBufferPtr->TexIndex = texIndex;
 		s_SceneData.QuadVertexBufferPtr->Scale = scale;
 		s_SceneData.QuadVertexBufferPtr++;
 
-		s_SceneData.QuadVertexBufferPtr->Position = { position.x, position.y + size.y, 0.0f };
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[3];
 		s_SceneData.QuadVertexBufferPtr->Color = color;
 		s_SceneData.QuadVertexBufferPtr->TexCoord = { 0.0f, 1.0f };
 		s_SceneData.QuadVertexBufferPtr->TexIndex = texIndex;
@@ -191,6 +218,8 @@ namespace Dessert {
 		s_SceneData.QuadVertexBufferPtr++;
 
 		s_SceneData.quadIndexCount += 6;
+
+		s_SceneData.Stats.QuadCount++;
 
 		//s_SceneData.TextureShader->SetFloat("u_TileMultiplier", 1.0f);
 		//s_SceneData.WhiteTexture->Bind();
@@ -213,6 +242,8 @@ namespace Dessert {
 	{
 		DGE_PROFILE_FUNCTION();
 
+		if (s_SceneData.quadIndexCount >= Renderer2DData::maxIndices)
+			FlushAndReset();
 		//constexpr glm::vec4 color = { 1.0f ,1.0f, 1.0f, 1.0f };
 
 		float textureIndex = 0.0f;
@@ -233,28 +264,31 @@ namespace Dessert {
 			s_SceneData.TextureSlotIndex++;
 		}
 
-		s_SceneData.QuadVertexBufferPtr->Position = position;
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
+			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[0];
 		s_SceneData.QuadVertexBufferPtr->Color = color;
 		s_SceneData.QuadVertexBufferPtr->TexCoord = { 0.0f, 0.0f };
 		s_SceneData.QuadVertexBufferPtr->TexIndex = textureIndex;
 		s_SceneData.QuadVertexBufferPtr->Scale = tileMultiplier;
 		s_SceneData.QuadVertexBufferPtr++;
 
-		s_SceneData.QuadVertexBufferPtr->Position = { position.x + size.x, position.y, 0.0f };
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[1];
 		s_SceneData.QuadVertexBufferPtr->Color = color;
 		s_SceneData.QuadVertexBufferPtr->TexCoord = { 1.0f, 0.0f };
 		s_SceneData.QuadVertexBufferPtr->TexIndex = textureIndex;
 		s_SceneData.QuadVertexBufferPtr->Scale = tileMultiplier;
 		s_SceneData.QuadVertexBufferPtr++;
 
-		s_SceneData.QuadVertexBufferPtr->Position = { position.x + size.x, position.y + size.y, 0.0f };
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[2];
 		s_SceneData.QuadVertexBufferPtr->Color = color;
 		s_SceneData.QuadVertexBufferPtr->TexCoord = { 1.0f, 1.0f };
 		s_SceneData.QuadVertexBufferPtr->TexIndex = textureIndex;
 		s_SceneData.QuadVertexBufferPtr->Scale = tileMultiplier;
 		s_SceneData.QuadVertexBufferPtr++;
 
-		s_SceneData.QuadVertexBufferPtr->Position = { position.x, position.y + size.y, 0.0f };
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[3];
 		s_SceneData.QuadVertexBufferPtr->Color = color;
 		s_SceneData.QuadVertexBufferPtr->TexCoord = { 0.0f, 1.0f };
 		s_SceneData.QuadVertexBufferPtr->TexIndex = textureIndex;
@@ -262,6 +296,8 @@ namespace Dessert {
 		s_SceneData.QuadVertexBufferPtr++;
 
 		s_SceneData.quadIndexCount += 6;
+
+		s_SceneData.Stats.QuadCount++;
 
 		//s_SceneData.TextureShader->SetFloat4("u_Color", color);
 		//s_SceneData.TextureShader->SetFloat("u_TileMultiplier", tileMultiplier);
@@ -286,7 +322,48 @@ namespace Dessert {
 	{
 		DGE_PROFILE_FUNCTION();
 
-		s_SceneData.TextureShader->SetFloat4("u_Color", color);
+		if (s_SceneData.quadIndexCount >= Renderer2DData::maxIndices)
+			FlushAndReset();
+
+		const float textureIndex = 0.0f; // white texture
+		const float scale = 1.0f;
+
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
+			* glm::rotate(glm::mat4(1.0f), glm::radians(rotation), { 0.0f, 0.0f, 1.0f })
+			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[0];
+		s_SceneData.QuadVertexBufferPtr->Color = color;
+		s_SceneData.QuadVertexBufferPtr->TexCoord = { 0.0f, 0.0f };
+		s_SceneData.QuadVertexBufferPtr->TexIndex = textureIndex;
+		s_SceneData.QuadVertexBufferPtr->Scale = scale;
+		s_SceneData.QuadVertexBufferPtr++;
+
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[1];
+		s_SceneData.QuadVertexBufferPtr->Color = color;
+		s_SceneData.QuadVertexBufferPtr->TexCoord = { 1.0f, 0.0f };
+		s_SceneData.QuadVertexBufferPtr->TexIndex = textureIndex;
+		s_SceneData.QuadVertexBufferPtr->Scale = scale;
+		s_SceneData.QuadVertexBufferPtr++;
+
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[2];
+		s_SceneData.QuadVertexBufferPtr->Color = color;
+		s_SceneData.QuadVertexBufferPtr->TexCoord = { 1.0f, 1.0f };
+		s_SceneData.QuadVertexBufferPtr->TexIndex = textureIndex;
+		s_SceneData.QuadVertexBufferPtr->Scale = scale;
+		s_SceneData.QuadVertexBufferPtr++;
+
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[3];
+		s_SceneData.QuadVertexBufferPtr->Color = color;
+		s_SceneData.QuadVertexBufferPtr->TexCoord = { 0.0f, 1.0f };
+		s_SceneData.QuadVertexBufferPtr->TexIndex = textureIndex;
+		s_SceneData.QuadVertexBufferPtr->Scale = scale;
+		s_SceneData.QuadVertexBufferPtr++;
+
+		s_SceneData.quadIndexCount += 6;
+
+		s_SceneData.Stats.QuadCount++;
+		/*s_SceneData.TextureShader->SetFloat4("u_Color", color);
 		s_SceneData.TextureShader->SetFloat("u_TileMultiplier", 1.0f);
 		s_SceneData.WhiteTexture->Bind();
 
@@ -297,7 +374,7 @@ namespace Dessert {
 		s_SceneData.TextureShader->SetMat4("u_Transform", transform);
 
 		s_SceneData.QuadVertexArray->Bind();
-		RenderCommand::DrawIndexed(s_SceneData.QuadVertexArray);
+		RenderCommand::DrawIndexed(s_SceneData.QuadVertexArray);*/
 	}
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const Ref<Texture2D>& texture, float tileMultiplier, const glm::vec4& color)
@@ -309,18 +386,85 @@ namespace Dessert {
 	{
 		DGE_PROFILE_FUNCTION();
 
-		s_SceneData.TextureShader->SetFloat4("u_Color", color);
-		s_SceneData.TextureShader->SetFloat("u_TileMultiplier", tileMultiplier);
-		texture->Bind();
+		if (s_SceneData.quadIndexCount >= Renderer2DData::maxIndices)
+			FlushAndReset();
 
-		glm::mat4 transform = glm::translate(glm::mat4(1.0), position)
-			* glm::rotate(glm::mat4(1.0f), rotation, { 0.0f, 0.0f, 1.0f })
+		float textureIndex = 0.0f;
+
+		for (uint32_t i = 1; i < s_SceneData.TextureSlotIndex; i++)
+		{
+			if (*s_SceneData.TextureSlots[i].get() == *texture.get())
+			{
+				textureIndex = (float)i;
+				break;
+			}
+		}
+
+		if (textureIndex == 0.0f)
+		{
+			textureIndex = (float)s_SceneData.TextureSlotIndex;
+			s_SceneData.TextureSlots[s_SceneData.TextureSlotIndex] = texture;
+			s_SceneData.TextureSlotIndex++;
+		}
+
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
+			* glm::rotate(glm::mat4(1.0f), glm::radians(rotation), { 0.0f, 0.0f, 1.0f })
 			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 
-		s_SceneData.TextureShader->SetMat4("u_Transform", transform);
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[0] ;
+		s_SceneData.QuadVertexBufferPtr->Color = color;
+		s_SceneData.QuadVertexBufferPtr->TexCoord = { 0.0f, 0.0f };
+		s_SceneData.QuadVertexBufferPtr->TexIndex = textureIndex;
+		s_SceneData.QuadVertexBufferPtr->Scale = tileMultiplier;
+		s_SceneData.QuadVertexBufferPtr++;
+
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[1] ;
+		s_SceneData.QuadVertexBufferPtr->Color = color;
+		s_SceneData.QuadVertexBufferPtr->TexCoord = { 1.0f, 0.0f };
+		s_SceneData.QuadVertexBufferPtr->TexIndex = textureIndex;
+		s_SceneData.QuadVertexBufferPtr->Scale = tileMultiplier;
+		s_SceneData.QuadVertexBufferPtr++;
+
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[2] ;
+		s_SceneData.QuadVertexBufferPtr->Color = color;
+		s_SceneData.QuadVertexBufferPtr->TexCoord = { 1.0f, 1.0f };
+		s_SceneData.QuadVertexBufferPtr->TexIndex = textureIndex;
+		s_SceneData.QuadVertexBufferPtr->Scale = tileMultiplier;
+		s_SceneData.QuadVertexBufferPtr++;
+
+		s_SceneData.QuadVertexBufferPtr->Position = transform * s_SceneData.QuadVertexPositions[3];
+		s_SceneData.QuadVertexBufferPtr->Color = color;
+		s_SceneData.QuadVertexBufferPtr->TexCoord = { 0.0f, 1.0f };
+		s_SceneData.QuadVertexBufferPtr->TexIndex = textureIndex;
+		s_SceneData.QuadVertexBufferPtr->Scale = tileMultiplier;
+		s_SceneData.QuadVertexBufferPtr++;
+
+		s_SceneData.quadIndexCount += 6;
+
+		s_SceneData.Stats.QuadCount++;
+
+		//s_SceneData.TextureShader->SetFloat4("u_Color", color);
+		//s_SceneData.TextureShader->SetFloat("u_TileMultiplier", tileMultiplier);
+		//texture->Bind();
+
+		//glm::mat4 transform = glm::translate(glm::mat4(1.0), position)
+		//	* glm::rotate(glm::mat4(1.0f), rotation, { 0.0f, 0.0f, 1.0f })
+		//	* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+
+		//s_SceneData.TextureShader->SetMat4("u_Transform", transform);
 
 
-		s_SceneData.QuadVertexArray->Bind();
-		RenderCommand::DrawIndexed(s_SceneData.QuadVertexArray);
+		//s_SceneData.QuadVertexArray->Bind();
+		//RenderCommand::DrawIndexed(s_SceneData.QuadVertexArray);
+	}
+
+	Renderer2D::DataStatistics Renderer2D::GetStats()
+	{
+		return s_SceneData.Stats;
+	}
+
+	void Renderer2D::ResetStats()
+	{
+		memset(&s_SceneData.Stats, 0, sizeof(DataStatistics));
 	}
 }
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -71,6 +71,7 @@ void Sandbox2D::OnUpdate(Dessert::Timestep delta)
 
 	m_CameraController.OnUpdate(delta);
 	
+	Dessert::Renderer2D::ResetStats();
 
 	{
 		DGE_PROFILE_SCOPE("Renderer Prep");
@@ -79,16 +80,29 @@ void Sandbox2D::OnUpdate(Dessert::Timestep delta)
 	}
 
 	{
+		static float rotation = 0.0f;
+		rotation += delta * 20.0f;
 		DGE_PROFILE_SCOPE("Renderer Draw");
 
 		Dessert::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
+		Dessert::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, rotation, { 0.5f, 0.2f, 0.3f, 1.0f });
 		Dessert::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-		//Dessert::Renderer2Dold::DrawRotatedQuad({ -0.25f, 0.25f }, { 0.2f, 0.2f }, glm::radians(45.0f) , { 0.8f, 0.2f, 0.3f, 1.0f });
-		Dessert::Renderer2D::DrawQuad({ -0.25f, 0.25f }, { 0.2f, 0.2f } , { 0.8f, 0.2f, 0.3f, 1.0f });
 		Dessert::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Dessert::Renderer2D::DrawQuad({ -0.5f, 0.5f, -0.1f }, { 4.0f, 4.0f }, m_Texture, 10.0f);
-		Dessert::Renderer2D::DrawQuad({ -0.5f, 0.5f, 0.0f }, { 1.0f, 1.0f }, m_Texture, 20.0f);
+		Dessert::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_Texture, 10.0f);
+		Dessert::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_Texture, 20.0f);
+
+
+		for (float y = -5.0f; y < 5.0f; y += 0.5f)
+		{
+			for (float x = -5.0f; x < 5.0f; x += 0.5f)
+			{
+				glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f };
+				Dessert::Renderer2D::DrawQuad({ x, y }, { 0.45f, 0.45f }, color);
+
+			}
+
+		}
 
 		Dessert::Renderer2D::EndScene();
 	}
@@ -105,6 +119,12 @@ void Sandbox2D::OnImGuiRender()
 
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 
+	auto stats = Dessert::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats: ");
+	ImGui::Text("Draw Calls : %d", stats.DrawCalls);
+	ImGui::Text("Quads count : %d", stats.QuadCount);
+	ImGui::Text("Vertices count : %d", stats.GetTotalVertexCount());
+	ImGui::Text("Indices count : %d", stats.GetTotalIndexCount());
 	/*for (auto& result : m_ProfileResults)
 	{
 		char label[50];
